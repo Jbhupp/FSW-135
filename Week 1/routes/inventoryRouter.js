33f82010c1204348b96fbb9c7d1e702fb68baa7b
github_Jbhupp <jenni.hupp@bryanuniversity.edu>
2022-11-30 22:39:52 -0500

    commit

---
+++ Week 1/routes/inventoryRouter.js
@@ -0,0 +1,20 @@
+const { response } = require('express')
+const express = require('express')
+const inventoryRouter = express.Router()
+const Inventory = require('../models/inventory.js')
+
+// Get All
+inventoryRouter.get("/", (req, res, next) => {
+    Inventory.find(
+        (err, items)=> {
+            if(err) {
+                res.status(500)
+                return next(err)
+            }
+            return res.status(200).send(items)
+        }
+    )
+    //res.send("Hello World")
+  })
+
+module.exports = inventoryRouter
